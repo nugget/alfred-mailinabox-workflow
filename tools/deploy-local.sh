2502de19c7312f8955d385a263e8e6d943d046f5
David McNett <nugget@macnugget.org>
2023-01-02 17:48:09 -0600

    Add comments to my deploy helper script

--- tools/deploy-local.sh
+++ tools/deploy-local.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# This script will copy all the relevant files from your current development
+# directory into the Alfred user workflow directory. This is only useful if
+# you're doing active development in a local clone of the git repository
+# and you want to push your local changes to the live workflow being used by
+# Alfred on your computer.
+#
+# Run it from repo root, as in:  ./tools/deploy-local.sh
+#
+# Not very well tested, but it works great on my machine!  :)
+#
 set -euo pipefail
 
 # Get location of workflows from Alfred preferences
@@ -16,13 +27,18 @@ if [ ! -e "script_filter.py" ]; then
 	exit 1
 fi
 
+# Figure out what bundleID corresponds to this workflow
 mybundleid=$(plutil -extract bundleid raw "info.plist")
 
+# Walk through all info.plist files in the Alfred sync directory
 find "$syncfolder" -name info.plist -print0 | while read -d $'\0' plist
 do
+	# If this info.plist file is for a workflow, see what bundleID it is
 	bundleid=$(plutil -extract bundleid raw "$plist" 2>/dev/null || true)
 
+	# Am I looking at the deployed version of this workflow?
 	if [ "$bundleid" == "$mybundleid" ]; then
+		# Yes -- let's overwrite the deployed workflow with these files
 		dst=$(dirname $plist)
 		manifest=("*.py")
 		install -cpSv ${manifest[@]} $dst/
@@ -30,4 +46,3 @@ do
 		exit 
 	fi
 done
-
